077937885bc43335e8e70d63b74f7d5a87754664
daief48 <daief20103048@gmail.com>
2023-05-06 01:29:11 +0600

    completed app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import { useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Clock from 'react-digital-clock';
+import Countdown from 'react-countdown';
+
 function App() {
-  const [num,setNum] = useState(0);
+  const [num, setNum] = useState(0);
   const incNum = () => {
     setNum(num + 1);
   }
   const decNum = () => {
-    if(num > 0){
+    if (num > 0) {
       setNum(num - 1);
-    }else{
+    } else {
       alert("Sorry, Zero Limit Reached")
       setNum(0);
     }
   }
   return (
-  <>
-    <div className="main_div">
-      <div className="center_div">
-        <h1> {num}</h1>
-        <div className="btn_div">
-          <button onClick={incNum} className="btn_green"><AddIcon/></button>
-          <button onClick={decNum} className="btn_red"><DeleteIcon/></button>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1> {num}</h1>
+          <div className="btn_div">
+            <Tooltip title="Add">
+              <Button onClick={incNum} className="btn_green"><AddIcon /></Button>
+            </Tooltip>
+            <Tooltip title="Delete">
+            <Button onClick={decNum} className="btn_red"><DeleteIcon /></Button>
+            </Tooltip>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-    );
+    </>
+  );
 }
 
 export default App;
